0ec0d3c880c295fcfee9e36baf2fac57b9562ab0
gonnagetapower <karpusha201@mail.ru>
2023-03-02 19:34:47 +0300

    addGlobalStore

--- vk-quiz/src/index.js
+++ vk-quiz/src/index.js
@@ -6,15 +6,20 @@ import App from './App.jsx';
 import { RouterContext } from '@happysanta/router';
 import { router } from './router.js';
 
+import { Provider } from 'react-redux';
+import store from './store/store.js';
+
 // Init VK  Mini App
 bridge.send('VKWebAppInit');
 
 ReactDOM.render(
   <RouterContext.Provider value={router}>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </RouterContext.Provider>,
   document.getElementById('root'),
 );
 if (process.env.NODE_ENV === 'development') {
-  import('./eruda').then(({ default: eruda }) => {}); //runtime download
+  import('./eruda').then(({ default: eruda }) => { }); //runtime download
 }
---
+++ vk-quiz/src/store/slices/appSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const appSlice = createSlice({
+    name: 'app',
+    initialState: {
+        money: '',
+        games: '',
+    },
+    reducers: {
+        
+    }
+})
