f103b6cf768fa50680424524cf652c6253873300
gonnagetapower <karpusha201@mail.ru>
2023-03-19 20:05:58 +0300

    addRoute

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,10 @@ const Router = require('express')
 const router = new Router()
 const categoryRouter = require('./categoryRouter')
 const quizRouter = require('./quizRouter')
+const userRouter = require('./userRouter.js')
 
 router.use('/', categoryRouter)
 router.use('/', quizRouter)
+router.use('/', userRouter)
 
 module.exports = router
