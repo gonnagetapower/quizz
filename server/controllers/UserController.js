7cb8322013697b08341e6aee6876c8681db9a617
gonnagetapower <karpusha201@mail.ru>
2023-03-19 20:03:53 +0300

    test

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,19 +1,24 @@
 const qs = require('querystring');
 const crypto = require('crypto');
 const verifyLaunchParams = require('../services/verify');
+const User = require('./../models/User.js')
 
 
 class UserController {
-    async sign(res, req) {
-        const token = req.body.token;
-        console.log(token)
-        if (token) {
-            const launchParams = decodeURIComponent(token.slice(token.indexOf('?') + 1));
-            console.log(launchParams)
-            const decode = verifyLaunchParams(launchParams, "1GvOf5adl7rmkKW00VND")
-            console.log(decode)
-            return res.json(decode)
-        }
+    // async sign(res, req) {
+    //     const token = req.body.token;
+    //     console.log(token)
+    //     if (token) {
+    //         const launchParams = decodeURIComponent(token.slice(token.indexOf('?') + 1));
+    //         console.log(launchParams)
+    //         const decode = verifyLaunchParams(launchParams, "1GvOf5adl7rmkKW00VND")
+    //         console.log(decode)
+    //         return res.json(decode)
+    //     }
+    // }
+    async test(res, req) {
+        const user = await User.find({ category: category })
+        return res.json(user)
     }
 }
 
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,6 +3,6 @@ const UserController = require('../controllers/UserController');
 const router = new Router()
 
 
-router.get('/user', UserController.sign)
+router.get('/user', UserController.test)
 
 module.exports = router;
