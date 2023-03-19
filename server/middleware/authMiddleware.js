bb4839347e6028d0a20444daaccde0ed6a7a5524
gonnagetapower <karpusha201@mail.ru>
2023-03-19 20:48:30 +0300

    addAuthMiddleWare

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,13 @@
+const verify = require('./../services/verify.js')
+
+module.exports = function (req, res, next) {
+    try {
+        const authHeader = req.headers.authorization;
+        const launchParams = decodeURIComponent(authHeader.slice(url.indexOf(' ') + 1));
+        const userData = verify(launchParams, '1GvOf5adl7rmkKW00VND')
+        req.user = userData;
+        next()
+    } catch (e) {
+
+    }
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,10 @@ const router = new Router()
 const categoryRouter = require('./categoryRouter')
 const quizRouter = require('./quizRouter')
 const userRouter = require('./userRouter.js')
+const authMiddleware = require('./../middleware/authMiddleware.js')
 
 router.use('/', categoryRouter)
 router.use('/', quizRouter)
-router.use('/', userRouter)
+router.use('/', authMiddleware, userRouter)
 
 module.exports = router
